fb0918668b2d8cae2738194ede8972edf2a65f79
Aitor <1726644+aaitor@users.noreply.github.com>
2022-06-17 19:20:08 +0200

    Moving CLI commands and options from yargs code to JSON file

---
+++ src/models/CLICommandsDefinition.ts
@@ -0,0 +1,47 @@
+import { PositionalOptionsType } from 'yargs'
+import * as CliCommands from '../commands'
+type CliCommands = typeof CliCommands
+
+export interface CLICommandsDefinition {
+  generalOptions: GeneralOption[]
+  commands: CommandDefinition[]
+}
+
+export interface CommandDefinition {
+  name: string
+  description: string
+  usage: string
+  subcommands: SubcommandDefinition[]
+}
+
+export interface SubcommandDefinition {
+  name: string
+  description: string
+  commandHandler: keyof CliCommands
+  positionalArguments?: PositionalArgument[]
+  optionalArguments?: OptionalArgument[]
+}
+
+export interface PositionalArgument {
+  name: string
+  type: PositionalOptionsType | undefined
+  default: string | undefined
+  description: string | undefined
+}
+
+export interface OptionalArgument {
+  name: string
+  type: PositionalOptionsType | undefined
+  default: string | undefined
+  description: string | undefined
+  demandOption?: boolean 
+  hidden?: boolean
+}
+
+export interface GeneralOption {
+  name: string
+  alias?: string | undefined
+  default: string | boolean
+  type: 'array' | 'count' | PositionalOptionsType | undefined
+  description: string
+}
