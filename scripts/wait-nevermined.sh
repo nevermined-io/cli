d3263dd739403dc74f6cab1ec55b5d9fe7b75e6d
Aitor <1726644+aaitor@users.noreply.github.com>
2022-06-27 12:59:21 +0200

    using `/tmp` folder in CI

--- scripts/wait-nevermined.sh
+++ scripts/wait-nevermined.sh
@@ -1,18 +1,22 @@
-#!/bin/bash
+#!/usr/bin/env bash
+set -emo pipefail
 
 # Wait for contracts migration and extract Keeper artifacts
 
 RETRY_COUNT=0
 COMMAND_STATUS=1
+LOCAL_CONF_DIR=${1:-~/.nevermined}
 
-mkdir -p node_modules/@nevermined-io/contracts/artifacts/
-mkdir -p node_modules/@nevermined-io/contracts/circuits/
+echo "Using $LOCAL_CONF_DIR for copying artifacts"
+
+mkdir -p $LOCAL_CONF_DIR/nevermined-contracts/artifacts/
+mkdir -p $LOCAL_CONF_DIR/nevermined-contracts/circuits/
 
 printf '\n\e[33m◯ Waiting for contracts to be generated...\e[0m\n'
 
 until [ $COMMAND_STATUS -eq 0 ] || [ $RETRY_COUNT -eq 120 ]; do
   nevermined_keeper_docker_id=$(docker container ls | grep nevermined-keeper-node | awk '{print $1}')
-  docker cp ${nevermined_keeper_docker_id}:/nevermined-contracts/artifacts/ready node_modules/@nevermined-io/contracts/artifacts/
+  docker cp ${nevermined_keeper_docker_id}:/nevermined-contracts/artifacts/ready $LOCAL_CONF_DIR/nevermined-contracts/artifacts/
   COMMAND_STATUS=$?
   sleep 5
   let RETRY_COUNT=RETRY_COUNT+1
@@ -26,10 +30,10 @@ fi
 
 printf '\e[32m✔ Found new contract artifacts.\e[0m\n'
 
-docker cp ${nevermined_keeper_docker_id}:/nevermined-contracts/artifacts/. node_modules/@nevermined-io/contracts/artifacts/
+docker cp ${nevermined_keeper_docker_id}:/nevermined-contracts/artifacts/. $LOCAL_CONF_DIR/nevermined-contracts/artifacts/
 
-docker cp ${nevermined_keeper_docker_id}:/nevermined-contracts/circuits/keytransfer.wasm node_modules/@nevermined-io/contracts/circuits/
-docker cp ${nevermined_keeper_docker_id}:/nevermined-contracts/circuits/keytransfer.zkey node_modules/@nevermined-io/contracts/circuits/
+docker cp ${nevermined_keeper_docker_id}:/nevermined-contracts/circuits/keytransfer.wasm $LOCAL_CONF_DIR/nevermined-contracts/circuits/
+docker cp ${nevermined_keeper_docker_id}:/nevermined-contracts/circuits/keytransfer.zkey $LOCAL_CONF_DIR/nevermined-contracts/circuits/
 
 
 printf '\e[32m✔ Copied new contract artifacts and circuits.\e[0m\n'
