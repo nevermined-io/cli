4bd7377deee07bee9e9abd6b90b7cffeba6cb69f
Aitor <1726644+aaitor@users.noreply.github.com>
2022-06-27 15:55:49 +0200

    testing with old script

--- scripts/wait-nevermined.sh
+++ scripts/wait-nevermined.sh
@@ -1,24 +1,18 @@
-#!/usr/bin/env bash
-set -emo pipefail
-
-export LC_ALL=en_US.UTF-8
+#!/bin/bash
 
 # Wait for contracts migration and extract Keeper artifacts
 
 RETRY_COUNT=0
 COMMAND_STATUS=1
-LOCAL_CONF_DIR=${LOCAL_CONF_DIR:-~/.nevermined}
 
-echo "Using $LOCAL_CONF_DIR for copying artifacts"
-
-mkdir -p $LOCAL_CONF_DIR/nevermined-contracts/artifacts/
-mkdir -p $LOCAL_CONF_DIR/nevermined-contracts/circuits/
+mkdir -p node_modules/@nevermined-io/contracts/artifacts/
+mkdir -p node_modules/@nevermined-io/contracts/circuits/
 
 printf '\n\e[33m◯ Waiting for contracts to be generated...\e[0m\n'
 
 until [ $COMMAND_STATUS -eq 0 ] || [ $RETRY_COUNT -eq 120 ]; do
   nevermined_keeper_docker_id=$(docker container ls | grep nevermined-keeper-node | awk '{print $1}')
-  docker cp ${nevermined_keeper_docker_id}:/nevermined-contracts/artifacts/ready $LOCAL_CONF_DIR/nevermined-contracts/artifacts/
+  docker cp ${nevermined_keeper_docker_id}:/nevermined-contracts/artifacts/ready node_modules/@nevermined-io/contracts/artifacts/
   COMMAND_STATUS=$?
   sleep 5
   let RETRY_COUNT=RETRY_COUNT+1
@@ -32,10 +26,10 @@ fi
 
 printf '\e[32m✔ Found new contract artifacts.\e[0m\n'
 
-docker cp ${nevermined_keeper_docker_id}:/nevermined-contracts/artifacts/. $LOCAL_CONF_DIR/nevermined-contracts/artifacts/
+docker cp ${nevermined_keeper_docker_id}:/nevermined-contracts/artifacts/. node_modules/@nevermined-io/contracts/artifacts/
 
-docker cp ${nevermined_keeper_docker_id}:/nevermined-contracts/circuits/keytransfer.wasm $LOCAL_CONF_DIR/nevermined-contracts/circuits/
-docker cp ${nevermined_keeper_docker_id}:/nevermined-contracts/circuits/keytransfer.zkey $LOCAL_CONF_DIR/nevermined-contracts/circuits/
+docker cp ${nevermined_keeper_docker_id}:/nevermined-contracts/circuits/keytransfer.wasm node_modules/@nevermined-io/contracts/circuits/
+docker cp ${nevermined_keeper_docker_id}:/nevermined-contracts/circuits/keytransfer.zkey node_modules/@nevermined-io/contracts/circuits/
 
 
 printf '\e[32m✔ Copied new contract artifacts and circuits.\e[0m\n'
