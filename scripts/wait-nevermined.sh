410a729048b5d5b9a8d2a9e8b531b6ce0d502510
Aitor <1726644+aaitor@users.noreply.github.com>
2022-06-27 16:10:50 +0200

    adapting wait nevermined script

--- scripts/wait-nevermined.sh
+++ scripts/wait-nevermined.sh
@@ -1,18 +1,25 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 # Wait for contracts migration and extract Keeper artifacts
 
 RETRY_COUNT=0
 COMMAND_STATUS=1
+CONF_DIR=${LOCAL_CONF_DIR:-~/.nevermined/nevermined-contracts}
 
-mkdir -p node_modules/@nevermined-io/contracts/artifacts/
-mkdir -p node_modules/@nevermined-io/contracts/circuits/
+ARTIFACTS_DIR=$CONF_DIR/artifacts/
+CIRCUITS_DIR=$CONF_DIR/circuits/
 
+mkdir -p $ARTIFACTS_DIR
+mkdir -p $CIRCUITS_DIR
+
+printf "\nUsing conf dir:$CONF_DIR\n"
 printf '\n\e[33m◯ Waiting for contracts to be generated...\e[0m\n'
+
+
 
 until [ $COMMAND_STATUS -eq 0 ] || [ $RETRY_COUNT -eq 120 ]; do
   nevermined_keeper_docker_id=$(docker container ls | grep nevermined-keeper-node | awk '{print $1}')
-  docker cp ${nevermined_keeper_docker_id}:/nevermined-contracts/artifacts/ready node_modules/@nevermined-io/contracts/artifacts/
+  docker cp ${nevermined_keeper_docker_id}:/nevermined-contracts/artifacts/ready $ARTIFACTS_DIR
   COMMAND_STATUS=$?
   sleep 5
   let RETRY_COUNT=RETRY_COUNT+1
@@ -26,10 +33,10 @@ fi
 
 printf '\e[32m✔ Found new contract artifacts.\e[0m\n'
 
-docker cp ${nevermined_keeper_docker_id}:/nevermined-contracts/artifacts/. node_modules/@nevermined-io/contracts/artifacts/
+docker cp ${nevermined_keeper_docker_id}:/nevermined-contracts/artifacts/. $ARTIFACTS_DIR
 
-docker cp ${nevermined_keeper_docker_id}:/nevermined-contracts/circuits/keytransfer.wasm node_modules/@nevermined-io/contracts/circuits/
-docker cp ${nevermined_keeper_docker_id}:/nevermined-contracts/circuits/keytransfer.zkey node_modules/@nevermined-io/contracts/circuits/
+docker cp ${nevermined_keeper_docker_id}:/nevermined-contracts/circuits/keytransfer.wasm $CIRCUITS_DIR
+docker cp ${nevermined_keeper_docker_id}:/nevermined-contracts/circuits/keytransfer.zkey $CIRCUITS_DIR
 
 
 printf '\e[32m✔ Copied new contract artifacts and circuits.\e[0m\n'
