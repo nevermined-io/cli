5210340c3e8fce7f9876aff222c589cffb5c8ada
Rodolphe Marques <marques.rodolphe@gmail.com>
2022-08-10 19:40:10 +0200

    improved ipfs to work with and without auth

--- src/utils/IpfsHelper.ts
+++ src/utils/IpfsHelper.ts
@@ -1,15 +1,20 @@
 const IpfsHttpClientLite = require('ipfs-http-client-lite')
+import chalk from 'chalk'
 import fetch from 'cross-fetch'
+import { getLogger } from 'log4js'
 
 const IPFS_GATEWAY = process.env.IPFS_GATEWAY || 'https://ipfs.infura.io:5001'
-
+export const logger = getLogger()
 export default class IpfsHelper {
   private static authToken: string
 
   public static async add(content: any): Promise<string> {
+    const authToken = this.getAuthToken()
     const ipfs = IpfsHttpClientLite({
       apiUrl: IPFS_GATEWAY,
-      headers: { Authorization: `Basic ${this.getAuthToken()}` }
+      ...(authToken && {
+        headers: { Authorization: `Basic ${authToken}` }
+      })
     })
     const addResult = await ipfs.add(content)
     return addResult[0].hash
@@ -17,9 +22,12 @@ export default class IpfsHelper {
 
   public static async get(cid: string): Promise<string> {
     const url = IPFS_GATEWAY + '/api/v0/cat?arg=' + cid.replace('cid://', '')
+    const authToken = this.getAuthToken()
     var options = {
       method: 'POST',
-      headers: { Authorization: `Basic ${this.getAuthToken()}` }
+      ...(authToken && {
+        headers: { Authorization: `Basic ${authToken}` }
+      })
     }
 
     return fetch(url, options)
@@ -36,14 +44,17 @@ export default class IpfsHelper {
       })
   }
 
-  private static getAuthToken(): string {
+  private static getAuthToken(): string | undefined {
     if (!this.authToken) {
       const { IPFS_PROJECT_ID, IPFS_PROJECT_SECRET } = process.env
 
       if (!IPFS_PROJECT_ID || !IPFS_PROJECT_SECRET) {
-        throw new Error(
-          'Infura IPFS_PROJECT_ID and IPFS_PROJECT_SECRET need to be set'
+        logger.warn(
+          chalk.red(
+            `\nWARNING: Infura IPFS_PROJECT_ID and IPFS_PROJECT_SECRET are not set - disabling ipfs auth`
+          )
         )
+        return undefined
       } else {
         this.authToken = Buffer.from(
           `${IPFS_PROJECT_ID}:${IPFS_PROJECT_SECRET}`
