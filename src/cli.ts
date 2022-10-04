035febb701ecad360f44179410449dadabcefb77
josepablofm78 <jp@keyko.io>
2022-10-04 09:05:29 +0200

    solves double escape characters in json output

--- src/cli.ts
+++ src/cli.ts
@@ -116,8 +116,9 @@ const cmdHandler = async (
       logger
     )
     if (argv.json) {
-      logger.mark(JSON.stringify(executionOutput))
+      logger.mark(executionOutput)
     }
+    
     if (executionOutput.status > 0) {
       logger.error(`Command error: ${executionOutput.errorMessage}`)
     }
