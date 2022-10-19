060cc88bf8489554547806fac57db54f22afaa60
Aitor <1726644+aaitor@users.noreply.github.com>
2022-10-19 14:04:45 +0200

    Fixing Polygon urls

--- src/commands/assets/getAsset.ts
+++ src/commands/assets/getAsset.ts
@@ -5,6 +5,9 @@ import readline from 'readline'
 import { Logger } from 'log4js'
 import { ExecutionOutput } from '../../models/ExecutionOutput'
 import { ConfigEntry } from '../../models/ConfigDefinition'
+import { Dtp } from '@nevermined-io/nevermined-sdk-dtp/dist/Dtp'
+import { InstantiableConfig } from '@nevermined-io/nevermined-sdk-js/dist/node/Instantiable.abstract'
+import { CryptoConfig } from '@nevermined-io/nevermined-sdk-dtp/dist/utils'
 
 readline.createInterface({
   input: process.stdin,
@@ -23,13 +26,19 @@ export const getAsset = async (
   let agreementId
 
   // TODO: Enable DTP when `sdk-dtp` is ready
-  // const instanceConfig: InstantiableConfig = {
-  //   nevermined: nvm,
-  //   config: config.nvm,
-  //   artifactsFolder: config.nvm.artifactsFolder
-  // }
+  const instanceConfig: InstantiableConfig = {
+    nevermined: nvm,
+    config: config.nvm,
+    artifactsFolder: config.nvm.artifactsFolder
+  }
 
-  // const keyTransfer = await makeKeyTransfer()
+  const gatewayInfo = await nvm.gateway.getGatewayInfo()
+  const cryptoConfig: CryptoConfig = {
+    provider_key: '',
+    provider_password: '',
+    provider_rsa_public: gatewayInfo['rsa-public-key'],
+    provider_rsa_private: ''
+  }
 
   logger.debug(chalk.dim(`Using account: '${account.getId()}'`))
 
@@ -44,22 +53,23 @@ export const getAsset = async (
     chalk.dim(`Downloading asset: ${did} with agreement id: ${agreementId}`)
   )
 
-  // if (password) {
-  //   const dtp = await Dtp.getInstance(instanceConfig, cryptoConfig)
-  //   const key = await dtp.consumeProof(agreementId, did, account)
-  //   results = Buffer.from(key as any, 'hex').toString()
-  //   logger.info(`Got password ${results}`)
-  // } else {
-  const path = await nvm.assets.consume(
-    agreementId,
-    did,
-    account,
-    argv.path,
-    argv.fileIndex
-  )
-  logger.info(chalk.dim(`Files downloaded to: ${path}`))
-  const results = path
-  // }
+  let results
+  if (argv.password) {
+    const dtp = await Dtp.getInstance(instanceConfig, cryptoConfig)
+    const key = await dtp.consumeProof(agreementId, did, account)
+    results = Buffer.from(key as any, 'hex').toString()
+    logger.info(`Got password ${results}`)
+  } else {
+    const path = await nvm.assets.consume(
+      agreementId,
+      did,
+      account,
+      argv.path,
+      argv.fileIndex
+    )
+    logger.info(chalk.dim(`Files downloaded to: ${path}`))
+    results = path
+  }
 
   return {
     status: StatusCodes.OK,
