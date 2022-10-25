64e1228f22153a4dfc21704dafbf22c6bb279262
Aitor <1726644+aaitor@users.noreply.github.com>
2022-10-25 14:57:19 +0200

    using `logger` to generate a proper output

--- src/commands/utils/upload.ts
+++ src/commands/utils/upload.ts
@@ -2,24 +2,49 @@ import { Account, Nevermined } from '@nevermined-io/nevermined-sdk-js'
 import fs from 'fs'
 import { ExecutionOutput } from '../../models/ExecutionOutput'
 import { StatusCodes } from '../../utils'
+import { ConfigEntry } from '../../models/ConfigDefinition'
+import { Logger } from 'log4js'
+import chalk from 'chalk'
 
 export const uploadFile = async (
   nvm: Nevermined,
   account: Account,
+  // eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types, @typescript-eslint/no-explicit-any
   argv: any,
+  config: ConfigEntry,
+  logger: Logger  
 ): Promise<ExecutionOutput> => {
   const { file, encrypt } = argv
-  const stream = fs.createReadStream(file)
-  const { url, password } = await nvm.files.uploadFilecoin(stream, encrypt)
-  console.info(`URL: ${url}`)
-  if (password) {
-    console.info(`Password: ${password}`)
+
+  logger.info(chalk.dim('Uploading File to Filecoin ...'))
+
+  if (!fs.existsSync(file))
+    return {
+      status: StatusCodes.ERROR,
+      errorMessage: `Unable to open ${file}`
+    }
+
+  try {
+    const stream = fs.createReadStream(file)
+    const { url, password } = await nvm.files.uploadFilecoin(stream, encrypt)
+    logger.info(`File uploaded to URL: ${url}`)
+    if (password) {
+      logger.info(`Password: ${password}`)
+    }
+    return {
+      status: StatusCodes.OK,
+      results: JSON.stringify({
+        url,
+        password
+      })
+    }
+  } catch (error) {
+    return {
+      status: StatusCodes.ERROR,
+      errorMessage: `Unable to upload file: ${
+        (error as Error).message
+      }`
+    }
   }
-  return {
-    status: StatusCodes.OK,
-    results: JSON.stringify({
-      url,
-      password
-    })
-  }
+
 }
