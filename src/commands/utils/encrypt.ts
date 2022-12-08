6c53689d47e888ea6f2822d4ec6621f3bffcab4e
Sami Mäkelä <mrsmkl@gmail.com>
2022-12-08 16:01:45 +0200

    adding intro message

--- src/commands/utils/encrypt.ts
+++ src/commands/utils/encrypt.ts
@@ -5,6 +5,7 @@ import { Logger } from 'log4js'
 import { StatusCodes } from '../../utils'
 import { ConfigEntry } from '../../models/ConfigDefinition'
 import { aes_encryption_256 } from '@nevermined-io/nevermined-sdk-dtp/dist/utils'
+import chalk from 'chalk'
 
 export const encryptFile = async (
   nvm: Nevermined,
@@ -13,6 +14,7 @@ export const encryptFile = async (
   config: ConfigEntry,
   logger: Logger
 ): Promise<ExecutionOutput> => {
+  logger.info(chalk.dim(`Encrypting file`))
   const { file, password } = argv
   const encrypted = fs.readFileSync(file).toString('binary')
   const decrypted = aes_encryption_256(encrypted, password)
