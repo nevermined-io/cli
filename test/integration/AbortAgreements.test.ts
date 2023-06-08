b1501c433ddfc1eb06d2a567b75082b6950318da
Aitor <1726644+aaitor@users.noreply.github.com>
2023-06-08 14:33:58 +0200

    ci: simplifying tests

--- test/integration/AbortAgreements.test.ts
+++ test/integration/AbortAgreements.test.ts
@@ -1,100 +1,38 @@
-import { execOpts, baseCommands } from '../helpers/Config'
+import { execOpts, baseCommands, metadataConfig } from '../helpers/Config'
 import execCommand from '../helpers/ExecCommand'
-import { Account, AssetPrice, BigNumber, ContractHandler, DDO, NFTAttributes, Nevermined, Nft721Contract, TransferNFT721Condition, ZeroAddress, generateId } from '@nevermined-io/sdk'
-import { config } from '../config'
-import { getMetadata } from '../helpers/ddo-metadata-generator'
-import { ethers } from 'ethers'
-import { JWTPayload, decodeJwt } from 'jose'
-import { parseNFTOrderAgreementId } from '../helpers/StdoutParser'
-import { ARTIFACTS_PATH } from '../../src/utils'
+import { generateId } from '@nevermined-io/sdk'
+import { parseAddressOfContractDeployed, parseDIDFromNewNFT, parseNFTOrderAgreementId } from '../helpers/StdoutParser'
 
 
 describe('Abort Agreements Testing', () => {
 
-  let publisher: Account
-  let collector1: Account
-
-  let nevermined: Nevermined
-  let transferNft721Condition: TransferNFT721Condition
-  let ddo: DDO
+  let did: string
   let agreementId: string
-  
-  const accessTimeout = 10
-  const accessTimelock = 3
-  const nftPrice = BigNumber.from(1)
-  const metadata = getMetadata(Math.random(), generateId())
-  let assetPrice: AssetPrice
-  let neverminedNodeAddress
-
-  let nft: ethers.Contract
-  let nftContract: Nft721Contract
-
-  let payload: JWTPayload
-
+  const accessTimeout = 5
+  const abiPath = 'test/resources/nfts/NFT721SubscriptionUpgradeable.json'
 
   beforeAll(async () => {
-    config.artifactsFolder = ARTIFACTS_PATH
-    nevermined = await Nevermined.getInstance(config)
+    
+    const deployCommand = `${baseCommands.nfts721.deploy} ${abiPath}  --accountIndex 0`
+    console.debug(`COMMAND: ${deployCommand}`)
 
-    ;[collector1, publisher,,, ] = await nevermined.accounts.list()
+    const stdout = execCommand(deployCommand, execOpts) 
+    expect(stdout.includes(`Contract deployed into address`))
+    const nftAddress = parseAddressOfContractDeployed(stdout)
 
-    const networkName = (await nevermined.keeper.getNetworkName()).toLowerCase()
-    const erc721ABI = await ContractHandler.getABI(
-      'NFT721Upgradeable',
-      config.artifactsFolder,
-      networkName,
-    )
+    const registerAssetCommand = `${baseCommands.nfts721.create} ${nftAddress}  --accountIndex 0 --name " NFTs 721 test ${metadataConfig.name}" --author "${metadataConfig.author}" --price "${metadataConfig.price}" --urls ${metadataConfig.url} --contentType ${metadataConfig.contentType} --transfer false`
+    console.debug(`COMMAND: ${registerAssetCommand}`)
 
-    nft = await nevermined.utils.contractHandler.deployAbi(erc721ABI, publisher, [
-      publisher.getId(),
-      nevermined.keeper.didRegistry.address,
-      'NFT721',
-      'NVM',
-      '',
-      '0',
-    ])
+    const registerStdout = execCommand(registerAssetCommand, execOpts)
 
-    nftContract = await Nft721Contract.getInstance(
-      (nevermined.keeper as any).instanceConfig,
-      nft.address,
-    )
+    console.debug(`STDOUT: ${registerStdout}`)
+    did = parseDIDFromNewNFT(registerStdout)
 
-    await nevermined.contracts.loadNft721(nftContract.address)
-
-    const clientAssertion = await nevermined.utils.jwt.generateClientAssertion(publisher)
-
-    await nevermined.services.marketplace.login(clientAssertion)
-    // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
-    payload = decodeJwt(config.marketplaceAuthToken!)
-    neverminedNodeAddress = await nevermined.services.node.getProviderAddress()
-
-    metadata.userId = payload.sub
-
-    // conditions
-    ;({ transferNft721Condition } = nevermined.keeper.conditions)
-
-    assetPrice = new AssetPrice()
-      .setReceiver(publisher.getId(), nftPrice)
-      .setTokenAddress(ZeroAddress)
-
-    await nftContract.grantOperatorRole(transferNft721Condition.address, publisher)
-
-    const nftAttributes = NFTAttributes.getNFT721Instance({
-      metadata,
-      price: assetPrice,
-      providers: [neverminedNodeAddress],
-      serviceTypes: ['nft-sales', 'nft-access'],
-      nftContractAddress: nftContract.address,
-      fulfillAccessTimeout: accessTimeout,
-      fulfillAccessTimelock: accessTimelock,
-    })
-    ddo = await nevermined.nfts721.create(nftAttributes, publisher)
-        
   })
 
   test('Abort an agreement', async () => {
-    const did = ddo.id
-    console.log(`Ordering Asset: ${ddo.id}`)
+    
+    console.log(`Ordering Asset: ${did}`)
     const orderCommand = `${baseCommands.nfts721.order} "${did}"  `
     console.debug(`COMMAND: ${orderCommand}`)
     const stdout = execCommand(orderCommand, execOpts)
@@ -104,7 +42,7 @@ describe('Abort Agreements Testing', () => {
     console.log(`Asset ordered: ${agreementId}`)
     expect(agreementId).toBeDefined()
     
-    await mineBlocks(nevermined, collector1, accessTimeout + 1)
+    await mineBlocks(did, accessTimeout + 1)
 
     const abortAgreementsCommand = `${baseCommands.agreements.abort} ${agreementId}`
     const stdoutAbort = execCommand(abortAgreementsCommand, execOpts)
@@ -122,16 +60,10 @@ describe('Abort Agreements Testing', () => {
   })  
 })
 
-export async function mineBlocks(nevermined: Nevermined, account: Account, blocksToWait: number) {
+export async function mineBlocks(did: string, blocksToWait: number) {
   for (let index = 0; index < blocksToWait; index++) {
-    await nevermined.provenance.used(
-      generateId(),
-      generateId(),
-      account.getId(),
-      account.getId(),
-      ethers.utils.hexZeroPad('0x0', 32),
-      'mining',
-      account,
-    )
+    const activityId = generateId()
+    const registerCommand = `${baseCommands.provenance.register} ${did} --accountIndex 0 --method used --agentId "${execOpts.accounts[0]}" --activityId "${activityId}" `
+    execCommand(registerCommand, execOpts)
   }
 }
